3cc7e02cfdf9554896000cd71f663b2731fe6a1c
Karthika Anil <karthikaa10z@gmail.com>
2026-02-18 18:20:22 +0530

    Displaying a list of contact links in the sidebar

--- app/contacts/layout.js
+++ app/contacts/layout.js
@@ -1,10 +1,12 @@
 import { MenuLink } from "@/components/menu-link";
 import styles from "./layout.module.css";
+import { CONTACTS_MOCK } from "@/mock/contacts";
 
 export default function ContcatsLayout({ children }) {
   return (
     <div className={styles.Layout}>
       <aside className={styles.Sidebar}>
+        
         <h4 className={styles.Subtitle}>Links</h4>
         <nav className={styles.Menu}>
           <ul>
@@ -13,6 +15,23 @@ export default function ContcatsLayout({ children }) {
             </li>
           </ul>
         </nav>
+
+        <h4 className={styles.Subtitle}>Links</h4>
+        <nav className={styles.Menu}>
+          <ul>
+            {CONTACTS_MOCK.map((contact) => (
+              <li key={contact.id}>
+                <MenuLink href={`/contacts/${contact.id}`}>
+                  <div className={styles.Card}>
+                    <img src={contact.avatarUrl} />
+                    {contact.firstName} {contact.lastName}
+                  </div>
+                </MenuLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
       </aside>
 
       {children}
